ffbe2da50f595d2620bc87f29af8a691b2c629b3
Wilson Castillo <dc105677543@gmail.com>
2024-01-31 11:02:25 -0500

    commit

--- app/index.tsx
+++ app/index.tsx
@@ -23,69 +23,11 @@ export default function Home() {
           space="$4"
           maxWidth={600}
         >
-          <H1 textAlign="center">Welcome to Tamagui.</H1>
-          <Paragraph textAlign="center">
-            Here&apos;s a basic starter to show navigating from one screen to
-            another.
-          </Paragraph>
+          <H1 textAlign="center">Movie Making.</H1>
         </YStack>
 
-        <YStack space="$2.5">
-          <Button onPress={() => router.push("/users/testuser")}>
-            Go to user page
-          </Button>
-          <Button onPress={() => router.push("/tabs")}>Go to tabs page</Button>
-        </YStack>
-
-        <YStack space="$5">
-          <YGroup
-            bordered
-            separator={<Separator />}
-            theme="green"
-          >
-            <YGroup.Item>
-              <Link
-                asChild
-                href="https://twitter.com/natebirdman"
-                target="_blank"
-              >
-                <ListItem
-                  hoverTheme
-                  title="Nate"
-                  pressTheme
-                  icon={Twitter}
-                />
-              </Link>
-            </YGroup.Item>
-            <YGroup.Item>
-              <Link
-                asChild
-                href="https://github.com/tamagui/tamagui"
-                target="_blank"
-              >
-                <ListItem
-                  hoverTheme
-                  pressTheme
-                  title="Tamagui"
-                  icon={Github}
-                />
-              </Link>
-            </YGroup.Item>
-            <YGroup.Item>
-              <Link
-                asChild
-                href="https://github.com/ivopr/tamagui-expo"
-                target="_blank"
-              >
-                <ListItem
-                  hoverTheme
-                  pressTheme
-                  title="This Template"
-                  icon={Github}
-                />
-              </Link>
-            </YGroup.Item>
-          </YGroup>
+        <YStack space="$3">
+          <Button onPress={() => router.push("/tabs")}>Begin</Button>
         </YStack>
       </MyStack>
     </MySafeAreaView>
